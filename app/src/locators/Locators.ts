7aa1e25a439049618a01a5393acc4b5116f379e9
Romain Fromi <romain.fromi@gmail.com>
2026-04-28 17:17:10 +0200

    Remove PlayerColor

--- app/src/locators/Locators.ts
+++ app/src/locators/Locators.ts
@@ -1,6 +1,5 @@
 import { LocationType } from '@gamepark/deja-vu/material/LocationType'
 import { MaterialType } from '@gamepark/deja-vu/material/MaterialType'
-import { PlayerColor } from '@gamepark/deja-vu/PlayerColor'
 import { Locator } from '@gamepark/react-game'
 
-export const Locators: Partial<Record<LocationType, Locator<PlayerColor, MaterialType, LocationType>>> = {}
+export const Locators: Partial<Record<LocationType, Locator<number, MaterialType, LocationType>>> = {}
--- rules/src/DejaVuSetup.ts
+++ rules/src/DejaVuSetup.ts
@@ -3,13 +3,12 @@ import { DejaVuOptions } from './DejaVuOptions'
 import { DejaVuRules } from './DejaVuRules'
 import { LocationType } from './material/LocationType'
 import { MaterialType } from './material/MaterialType'
-import { PlayerColor } from './PlayerColor'
 import { RuleId } from './rules/RuleId'
 
 /**
  * This class creates a new Game based on the game options
  */
-export class DejaVuSetup extends MaterialGameSetup<PlayerColor, MaterialType, LocationType, DejaVuOptions> {
+export class DejaVuSetup extends MaterialGameSetup<number, MaterialType, LocationType, DejaVuOptions> {
   Rules = DejaVuRules
 
   setupMaterial(_options: DejaVuOptions) {
