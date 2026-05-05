dfc8b94f1f47bcdea1103e4a7850213a47ca3602
sylvestredavid <david.sylvestre.24@gmail.com>
2026-05-05 18:12:35 +0200

    display

--- app/src/GameDisplay.tsx
+++ app/src/GameDisplay.tsx
@@ -1,13 +1,12 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
-import { DevToolsHub, GameTable, GameTableNavigation } from '@gamepark/react-game'
+import { DevToolsHub, GameTable } from '@gamepark/react-game'
 import { PlayerPanels } from './panels/PlayerPanels'
 
 export function GameDisplay() {
-  const margin = { top: 7, left: 0, right: 30, bottom: 0 }
+  const margin = { top: 7, left: 0, right: 0, bottom: 0 }
   return (
-    <GameTable xMin={-50} xMax={50} yMin={-30} yMax={30} margin={margin} css={process.env.NODE_ENV === 'development' && tableBorder}>
-      <GameTableNavigation />
+    <GameTable xMin={-30} xMax={30} yMin={-15} yMax={15} margin={margin} zoom={false} css={process.env.NODE_ENV === 'development' && tableBorder}>
       <PlayerPanels />
       {process.env.NODE_ENV === 'development' && <DevToolsHub fabBottom="calc(1em + 6em * 1.7)" />}
     </GameTable>
--- app/src/material/InstinctTokenDescription.ts
+++ app/src/material/InstinctTokenDescription.ts
@@ -6,6 +6,8 @@ class InstinctTokenDescription extends TokenDescription {
   width = 4.94 / 2
   height = 9.4 / 2
 
+  transparency = true
+
   image = InstinctToken
   help = InstinctTokenHelp
 }
