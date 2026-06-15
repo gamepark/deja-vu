64f718cdd6e281bb08aeeea18dbbde0a718c8cc5
Romain Fromi <romain.fromi@gmail.com>
2026-06-15 14:18:11 +0200

    Improve game over header

--- app/src/headers/GameOverHeader.tsx
+++ app/src/headers/GameOverHeader.tsx
@@ -1,7 +1,7 @@
 /** @jsxImportSource @emotion/react */
 import { DejaVuRules } from '@gamepark/deja-vu/DejaVuRules'
 import { ScoreHelper } from '@gamepark/deja-vu/rules/helper/ScoreHelper'
-import { usePlayerName, useRankedPlayers, useRules } from '@gamepark/react-game'
+import { usePlayerId, usePlayerName, useRankedPlayers, useRules } from '@gamepark/react-game'
 import { Trans } from 'react-i18next'
 
 export const GameOverHeader = () => {
@@ -9,14 +9,16 @@ export const GameOverHeader = () => {
   const rankedPlayers = useRankedPlayers<number>()
   const winner = rankedPlayers[0]
   const winnerName = usePlayerName(winner.id)
+  const playerId = usePlayerId()
+  const itsMe = playerId !== undefined && winner.id === playerId
 
   const scoreHelper = new ScoreHelper(rules.game)
 
   const opponent = rules.game.players.find((p) => p !== winner.id)!
   if (scoreHelper.getTokenCount(opponent) === 0) {
-    return <Trans i18nKey="game.over.instinct" values={{ player: winnerName }} />
+    return <Trans i18nKey={itsMe ? 'game.over.instinct.you' : 'game.over.instinct.player'} values={{ player: winnerName }} />
   }
 
   const score = scoreHelper.getScore(winner.id)
-  return <Trans i18nKey="game.over.winner" values={{ player: winnerName, score }} />
+  return <Trans i18nKey={itsMe ? 'game.over.winner.you' : 'game.over.winner.player'} values={{ player: winnerName, score }} />
 }
